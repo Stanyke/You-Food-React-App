9bf68c9ca89c1fbaea1d734a0c6a3f6a2803a877
Stanley Chibuike <chibuike.stanley50@gmail.com>
2021-11-28 10:23:05 +0100

    Added filter page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import Quests from './pages/Quests';
 import OrderItem from './pages/OrderItem';
+import AllQuests from './pages/AllQuests';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/quests" element={<AllQuests />} />
           <Route path="/quests/:id" element={<Quests />} />
           <Route path="/orders/:id" element={<OrderItem />} />
         </Routes>
